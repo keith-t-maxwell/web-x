83d4e7f2aec455a86d6ebbc36c3e6b99b83ed366
keith-t-maxwell <46784452+keith-t-maxwell@users.noreply.github.com>
2019-02-26 17:13:51 +0300

    #Игра
    Файлы игры

---
+++ menu.js
@@ -0,0 +1,15 @@
+	var Menu = 
+		{ 
+			preload: function () 
+			{ 
+			
+				game.load.image('menu', 
+				'./assets/images/menu.png'): 
+			}, 
+				create: function () 
+				{ 
+				
+					this.add.sprite(0, 0 'menu'); 
+				};
+					
+		}
